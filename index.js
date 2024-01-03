3860f2214b86b19a003e83bdcb492fe416316bbe
medhaholla <holla.medha@gmail.com>
2024-01-03 11:55:19 +0530

    Implemented POST route '/weather' to handle city weather data retrieval

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const app = express();
-
+app.use(express.json());
 const PORT = process.env.PORT || 5052;
+const api_key = "380dfcb2e1616adb7ae5c5cea215000f";
+
+app.post("/weather", async (req, res) => {
+  const { cities } = req.body;
+
+  try {
+    const weatherFetch = cities.map(async (city) => {
+      const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`
+      );
+      const data = await response.json();
+
+      return { [city]: `${data.main.temp}°C` };
+    });
+
+    const results = await Promise.all(weatherFetch);
+    const weather = Object.assign({}, ...results);
+    res.json({ weather });
+  } catch (err) {
+    res.status(500).json({ error: "Error fetching weather data" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log("server started");
